c7e184bf473bd44bf742199621d3ed146126edc7
bifjhh <929160004@qq.com>
2017-12-20 06:22:02 +0800

    添加列表页面

--- locally-life/pages/list/list.js
+++ locally-life/pages/list/list.js
@@ -1,90 +1,66 @@
-// pages/index/index.js
+// pages/list/list.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    slides: [],
-    categories: []
+  
   },
 
-  /** 
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var _this=this;
-    wx.request({
-      url: 'https://locally.uieee.com/slides',
-      success:(res)=>{
-        let data = res.data;
-        this.setData({slides:data});
-        console.log(data);
-      }
-    })
-    // wx.request({
-    //     url:'https://api.douban.com/v2/movie/coming_soon',
-    //     header:{
-    //       'Content-Type':'json'
-    //     },
-    //     success: function(res) {
-    //       console.log(res)
-    //     }
-    //     // 发送异步请求不再是WEB那套AJAX
-    //     // 客户端开发  是没有跨域的
-    //     // 请求的地址必须在管理后台添加白名单
-    //     // 请求的域名必须备案，服务端必须采用HTTPS
-
-    // })
-
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
