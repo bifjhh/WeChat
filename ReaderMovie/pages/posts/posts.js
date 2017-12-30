375a8ca46cfb7d1efdf51d7266562eb361324b7d
bifjhh <929160004@qq.com>
2017-12-30 14:31:41 +0800

    使用wx-for 循环创建新闻列表

--- ReaderMovie/pages/posts/posts.js
+++ ReaderMovie/pages/posts/posts.js
@@ -5,14 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    posts_content:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var posts_content = [
+      {
+        date:"Sep 18 2016",
+        title:"正式虾肥蟹壮时",
+        post_img:"/images/avatar/1.png",
+        author_img:"/images/post/crab.png",
+        content: "本书覆盖 ES6 与上一个版本 ES5 的所有不同之处，对涉及的语法知识给予详细介绍，并给出大量简洁易懂的示例代码",
+        view_num:"112",
+        collect:"96"
+      },
+      {
+        date:"Sep 18 2017",
+        title:"正式虾肥蟹壮时",
+        post_img:"/images/avatar/1.png",
+        author_img:"/images/post/crab.png",
+        content: "本书覆盖 ES6 与上一个版本 ES5 的所有不同之处，对涉及的语法知识给予详细介绍，并给出大量简洁易懂的示例代码",
+        view_num:"112",
+        collect:"96"
+      },
+      {
+        date:"Sep 18 20186",
+        title:"正式虾肥蟹壮时",
+        post_img:"/images/avatar/1.png",
+        author_img:"/images/post/crab.png",
+        content: "本书覆盖 ES6 与上一个版本 ES5 的所有不同之处，对涉及的语法知识给予详细介绍，并给出大量简洁易懂的示例代码",
+        view_num:"112",
+        collect:"96"
+      },
+  ]
+    this.setData({
+      posts_content
+    });
   },
 
   /**
