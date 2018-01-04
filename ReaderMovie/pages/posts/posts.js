4da392eefdd55451e8411d6d0f642b19bb4f97c2
bifjhh <15518730711@163.com>
2018-01-04 09:08:46 +0800

    完成新闻裂变，使用使用require 方法加载外部js文件

--- ReaderMovie/pages/posts/posts.js
+++ ReaderMovie/pages/posts/posts.js
@@ -4,18 +4,10 @@
 // ES6
 import postsData from '../data/posts-data.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     posts_content:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+ onLoad: function (options) {
     this.setData({
       posts_content: postsData.local_database
     });
@@ -39,55 +31,5 @@ Page({
         // complete
       }
     }) */
-   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
